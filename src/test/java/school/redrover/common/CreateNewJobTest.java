0157859b323459ef14f41612285a210a7f4bdd87
yathane <hisnarei@gmail.com>
2026-04-26 22:01:39 +0300

    Test for new job creation added. Asserts section not finished.

--- src/test/java/school/redrover/common/CreateNewJobTest.java
+++ src/test/java/school/redrover/common/CreateNewJobTest.java
@@ -2,6 +2,8 @@ package school.redrover.common;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CreateNewJobTest extends BaseTest {
@@ -9,21 +11,23 @@ public class CreateNewJobTest extends BaseTest {
     public void testCreateNewJob() {
         getDriver().findElement(By.xpath("//a[@href = 'newJob']")).click();
 
-        WebElement itemNameField = getDriver().findElement(By.id("#name"));
+        WebElement itemNameField = getDriver().findElement(By.id("name"));
         itemNameField.click();
         itemNameField.sendKeys("test_job_1");
 
-        getDriver().findElement(
-                By.xpath("//li[@class = 'jenkins_branch_OrganizationFolder']")).click();
+        getDriver().findElement(By.xpath("//li[@class = 'jenkins_branch_OrganizationFolder']")).click();
 
-        getDriver().findElement(By.id("#ok-button")).click();
+        getDriver().findElement(By.id("ok-button")).click();
 
-        getDriver().findElement(By.id("#jenkins-head-icon"));
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("page-header")));
+        getDriver().findElement(By.id("jenkins-head-icon")).click();
 
+        WebElement table = getDriver().findElement(By.xpath("//tr/td[3]"));
+        WebElement itemType = getDriver().findElement(By.xpath("//*[@title='Organization Folder']"));
 
-        //Assert
-        //need to check that - same folder name displayed (test_job_1)
-        //                   - same element type displayed (OrganizationFolder)
+        Assert.assertEquals(table.getText(), "test_job_1");
+        Assert.assertTrue(itemType.isDisplayed());
+
 
     }
 }
